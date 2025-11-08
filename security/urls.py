a88d6c01e37d3437ba5bdd16dd63476dfb3910d6
Sistema de Gerenciamento de Clínicas <iclynikax@gmail.com>
2025-11-08 01:03:58 -0300

    Add files via upload

--- security/urls.py
+++ security/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.fnct_security, name="url_security"),
     path('sobre/', views.fnct_scrty_sobre, name="url_scrty_sobre"),
+    path('test/', views.fnct_scrty_test, name="url_scrty_test"),
     path('acessos/', views.fnct_scrty_acessos, name="url_scrty_acessos"),
     path('grafico_dados/', views.grafico_dados, name='grafico_dados'),
 ]
